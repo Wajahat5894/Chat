05cb3008a31823b782ff65ce6ca8a45de6748dae
Wajahat Ahmad <wajahatahmad@minivonwajahat.speedport.ip>
2023-04-09 15:08:20 +0200

    hallo

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Avatar} from '@mui/material'
+
+const Cat = () => {
+  return (
+    < Avatar src = "https://publish.purewow.net/wp-content/uploads/sites/2/2021/10/laziest-cat-breeds-cat.jpg" />
+  )
+}
+
+export default Cat
